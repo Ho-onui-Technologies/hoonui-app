94e9710109e12054b77265db05e22c9bd830a97b
Brody <brodyuehara@gmail.com>
2023-06-01 21:03:56 -1000

    User upsert works

--- pages/api/webhooks/user.ts
+++ pages/api/webhooks/user.ts
@@ -2,7 +2,11 @@ import { IncomingHttpHeaders } from "http";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Webhook, WebhookRequiredHeaders } from "svix";
 import { buffer } from "micro";
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
+const supabaseKey = process.env.SUPABASE_API_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Disable the bodyParser so we can access the raw
 // request body for verification.
@@ -13,12 +17,6 @@ export const config = {
 };
 
 const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
-
-// Create a single supabase client for interacting with your database
-const supabaseUrl = process.env.SUPABASE_PROJECT_URL;
-const supabaseKey = process.env.SUPABASE_API_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
@@ -40,14 +38,32 @@ export default async function handler(
   const eventType: EventType = evt.type;
   if (eventType === "user.created" || eventType === "user.updated") {
     const { id, ...attributes } = evt.data;
-    const { error } = await supabase
-        .from('users')
-        .upsert([
-        { id: id, ...attributes },
-        ], { returning: 'minimal' }); // This option is set to reduce the amount of data returned by the call
+
+    // Extract first name, last name, and email address from attributes
+    const firstName = attributes.first_name;
+    const lastName = attributes.last_name;
+
+    // Handle cases where there might be more than one email address
+    // Here we just pick the first one but you might want to handle it differently
+    const emailAddress = attributes.email_addresses && attributes.email_addresses.length > 0
+      ? attributes.email_addresses[0].email_address
+      : '';
+
+    const { data, error } = await supabase
+      .from('users')
+      .upsert([
+        {
+          user_id: id,
+          first_name: firstName,
+          last_name: lastName,
+          email_address: emailAddress
+        },
+      ], { onConflict: 'user_id' })
 
     if (error) {
-        console.error('Error upserting data:', error);
+      console.error('Error upserting user:', error)
+    } else {
+      console.log('User upserted:', data)
     }
   }
 
@@ -61,7 +77,16 @@ type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
 // Generic (and naive) way for the Clerk event
 // payload type.
 type Event = {
-  data: Record<string, string | number>;
+  data: {
+    id: string;
+    attributes: {
+      first_name: string;
+      last_name: string;
+      email_addresses: { email_address: string }[];
+      [key: string]: any;
+    };
+    [key: string]: any;
+  };
   object: "event";
   type: EventType;
 };
