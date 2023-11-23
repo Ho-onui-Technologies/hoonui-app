3814ac6b9003078644e67ff32285f80cc3a65d63
Brody <45079557+brodyu@users.noreply.github.com>
2023-11-23 08:40:36 -1000

    Removed unused component files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import Container from '../components/container';
 import Intro from '../components/landing/landing';
 import Layout from '../components/layout';
-import { getAllPosts } from '../lib/api';
 import Head from 'next/head';
-import Post from '../interfaces/post';
 
-type Props = {
-  allPosts: Post[]
-}
-
-export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index() {
   return (
     <>
       <Layout>
@@ -24,19 +16,4 @@ export default function Index({ allPosts }: Props) {
       </Layout>
     </>
   )
-}
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
 }
